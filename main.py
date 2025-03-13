fd08eb5bafb852d480a37284969e787e27a0fbfc
eugene arida <eugene.13arida@gmail.com>
2025-03-13 09:58:53 +0800

    UPDATE

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
+    
 
     def __repr__(self):
         return f'<User {self.username}>'
