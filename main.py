b4027f60a85ddcba733faa3d86a74c1b88d3cf4c
eugene arida <eugene.13arida@gmail.com>
2025-05-05 23:56:35 +0800

    test2321

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
     is_admin = db.Column(db.Boolean, default=False) 
-    profile_picture = db.Column(db.String(120), default='default.jpg') 
+    profile_picture = db.Column(db.String(120), default=False) 
     full_name = db.Column(db.String(120), nullable=True) 
     address = db.Column(db.String(255), nullable=True)
     contact_number = db.Column(db.String(15), nullable=True) 
@@ -150,6 +150,7 @@ def register():
         username = request.form.get('username')
         email = request.form.get('email')
         password = request.form.get('password')
+
 
         # Check if username or email already exists
         user_exists = User.query.filter_by(username=username).first()
